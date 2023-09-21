e81d981f30f6cda604208941040a2592299b8b80
SangWoo Park <136118540+angwoo0503@users.noreply.github.com>
2023-09-21 23:20:44 +0900

    Feat: 프로필 페이지 UI 추가

---
+++ CoreDataProject/ProfileViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileViewController.swift
+//  CoreDataProject
+//
+//  Created by t2023-m0061 on 2023/09/20.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
